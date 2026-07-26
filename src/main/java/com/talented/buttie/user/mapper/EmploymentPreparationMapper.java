2807da596e0ac775dce9cf30b3c2b19054807321
장예연 <sudpdj124@gmail.com>
2026-07-26 21:39:51 +0900

    BUT-24 Refactor: Mapper 변수명 축약어 수정

--- src/main/java/com/talented/buttie/user/mapper/EmploymentPreparationMapper.java
+++ src/main/java/com/talented/buttie/user/mapper/EmploymentPreparationMapper.java
@@ -5,5 +5,5 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface EmploymentPreparationMapper {
 
-    int updateEmploymentPreparation(@Param("employmentPreparation") EmploymentPreparationVO ep);
+    int updateEmploymentPreparation(@Param("employmentPreparation") EmploymentPreparationVO employmentPreparation);
 }
