061084de279564b7c5ad97af75e26fbe93c277e5
장예연 <sudpdj124@gmail.com>
2026-07-27 21:10:22 +0900

    BUT-37 Feature: 회원 프로필 조회 API 구현

--- src/main/java/com/talented/buttie/user/controller/UserController.java
+++ src/main/java/com/talented/buttie/user/controller/UserController.java
@@ -1,8 +1,16 @@
 package com.talented.buttie.user.controller;
 
+import com.talented.buttie.common.response.ApplicationResponse;
+import com.talented.buttie.user.domain.UserProfileVO;
+import com.talented.buttie.user.dto.response.GetUserProfileResponseDTO;
+import com.talented.buttie.user.service.UserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = "User")
@@ -10,4 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users/my")
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    @ApiOperation("회원 프로필 조회")
+    public ApplicationResponse<GetUserProfileResponseDTO> getUserProfile(
+        @ApiParam(value = "사용자 ID", required = true)
+        @RequestParam Long userId
+    ) {
+        UserProfileVO vo = userService.getUserProfile(userId);
+        return ApplicationResponse.onSuccess(GetUserProfileResponseDTO.from(vo));
+    }
 }
--- src/main/java/com/talented/buttie/user/service/UserService.java
+++ src/main/java/com/talented/buttie/user/service/UserService.java
@@ -1,9 +1,25 @@
 package com.talented.buttie.user.service;
 
+import com.talented.buttie.common.exception.ApplicationException;
+import com.talented.buttie.user.domain.UserProfileVO;
+import com.talented.buttie.user.exception.UserErrorCode;
+import com.talented.buttie.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final UserMapper userMapper;
+
+    public UserProfileVO getUserProfile(Long userId) {
+        UserProfileVO vo = userMapper.selectUserProfile(userId);
+
+        if (vo == null) {
+            throw ApplicationException.from(UserErrorCode.USER_NOT_FOUND);
+        }
+
+        return vo;
+    }
 }
