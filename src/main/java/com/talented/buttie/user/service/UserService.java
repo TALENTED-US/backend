9744b6b9e38ee9dfb7569ef95306413116204210
장예연 <sudpdj124@gmail.com>
2026-07-28 09:26:28 +0900

    BUT-24 취업 정보 저장 API 구현한다. (#42)
    * ãBUT-24 Feature: 취업 준비 정보 등록 ãAPI 구현
    
    * BUT-24 Feature: 취업 준비 정보 등록/수정 통합 및 예외처리
    
    * BUT-24 Refactor: Controller 응답을 UserPKResponseDTO로 변경
    
    * BUT-24 Refactor: ControVa@Valid ì추가
    
    * BUT-24 Refactor: CMapper 축약어(ep) 풀어서 작성
    
    * BUT-24 Refactor: VO 생성 로직 VO 내부로 이동 및 변수명 수정
    
    * BUT-24 Refactor: DTO에 Swagger 및 검증 어노테이션 추가
    
    * BUT-24 Refactor: Mapper 변수명 축약어 수정
    
    * BUT-24 Refactor: Service가 VO를 리턴하도록 변경
    
    * BUT-24 Test: 취업 준비 정보 저장 테스트코드 작성

--- src/main/java/com/talented/buttie/user/service/UserService.java
+++ src/main/java/com/talented/buttie/user/service/UserService.java
@@ -1,9 +1,26 @@
 package com.talented.buttie.user.service;
 
+import com.talented.buttie.user.domain.EmploymentPreparationVO;
+import com.talented.buttie.user.dto.request.UpdateEmploymentPreparationRequestDTO;
+import com.talented.buttie.user.mapper.EmploymentPreparationMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.talented.buttie.common.exception.ApplicationException;
+import com.talented.buttie.user.exception.UserErrorCode;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final EmploymentPreparationMapper employmentPreparationMapper;
+    public EmploymentPreparationVO saveEmploymentPreparation(Long userId, UpdateEmploymentPreparationRequestDTO request) {
+
+        EmploymentPreparationVO employmentPreparation =
+            EmploymentPreparationVO.createEmploymentPreparation(userId, request);
+
+        int updated = employmentPreparationMapper.updateEmploymentPreparation(employmentPreparation);
+        if (updated == 0) {
+            throw ApplicationException.from(UserErrorCode.EMPLOYMENT_PREPARATION_NOT_FOUND);
+        }
+        return employmentPreparation;
+    }
 }
