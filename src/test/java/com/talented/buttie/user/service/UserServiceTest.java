f7ab21f5c69afe326d232bc7d6a17862456a74a8
장예연 <sudpdj124@gmail.com>
2026-07-28 17:52:06 +0900

    BUT-27 Refactor: 불필요한 코드 제거

--- src/test/java/com/talented/buttie/user/service/UserServiceTest.java
+++ src/test/java/com/talented/buttie/user/service/UserServiceTest.java
@@ -82,8 +82,6 @@ class UserServiceTest {
             exception.getCode()
         );
     }
-<<<<<<< HEAD
-
     @Test
     @DisplayName("취업 준비 정보를 조회하면 해당 사용자의 정보를 반환한다.")
     void getEmploymentPreparation() {
@@ -125,6 +123,5 @@ class UserServiceTest {
             exception.getCode()
         );
     }
-=======
->>>>>>> origin/develop
+
 }
