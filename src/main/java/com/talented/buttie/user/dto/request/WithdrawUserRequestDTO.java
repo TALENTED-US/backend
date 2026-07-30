15b8aa578e90a9dc03a06a86a18ce853017c9617
장예연 <sudpdj124@gmail.com>
2026-07-30 10:08:08 +0900

    BUT-46 Rename: WithdrawUserRequestDTO 파일명 대소문자 수정

--- src/main/java/com/talented/buttie/user/dto/request/WithdrawUserRequestDTO.java
+++ src/main/java/com/talented/buttie/user/dto/request/WithdrawUserRequestDTO.java
@@ -4,10 +4,9 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @ApiModel(description = "회원 탈퇴 요청 DTO")
-public class withdrawUserRequestDTO {
+public class WithdrawUserRequestDTO {
 
     @ApiModelProperty(value = "비밀번호", example = "password1234", required = true)
     @NotNull(message = "비밀번호는 필수입니다.")
