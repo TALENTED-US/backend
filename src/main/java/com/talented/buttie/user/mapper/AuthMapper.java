c5e6e33a1ee7b9724a35df8b65d1705d62868884
hurrayPersimmon <rahel337@naver.com>
2026-07-31 08:25:14 +0900

    BUT-17 Feature: 회원가입 기능 구현

---
+++ src/main/java/com/talented/buttie/user/mapper/AuthMapper.java
@@ -0,0 +1,15 @@
+package com.talented.buttie.user.mapper;
+
+import com.talented.buttie.user.dto.request.auth.AuthSignUpRequestDTO;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface AuthMapper {
+    Long createUser(@Param("authSignUpRequestDTO") AuthSignUpRequestDTO authSignUpRequestDTO);
+    boolean existsByPhoneNumber(@Param("phoneNumber") String phoneNumber);
+    boolean existsByEmail(@Param("email") String email);
+    boolean existsByNickName(@NotBlank(message = "닉네임은 필수입니다.") @Size(min = 2, max = 20, message = "닉네임은 2~20자여야 합니다.") String userNickname);
+}
