5453d88ba662d3bf5c49938e7f4127f4dd3904a6
hurrayPersimmon <rahel337@naver.com>
2026-08-01 18:10:45 +0900

    BUT-55 Rename: Auth prefix 추가 및 Swagger 추가

--- src/main/java/com/talented/buttie/user/service/AuthReadService.java
+++ src/main/java/com/talented/buttie/user/service/AuthReadService.java
@@ -2,7 +2,7 @@ package com.talented.buttie.user.service;
 
 import com.talented.buttie.common.exception.ApplicationException;
 import com.talented.buttie.user.dto.request.auth.AuthLoginRequestDTO;
-import com.talented.buttie.user.dto.response.auth.TokenResponseDTO;
+import com.talented.buttie.user.dto.response.auth.AuthTokenResponseDTO;
 import com.talented.buttie.user.exception.AuthErrorCode;
 import com.talented.buttie.user.mapper.AuthMapper;
 import javax.validation.Valid;
@@ -20,7 +20,7 @@ public class AuthReadService {
     private final PasswordEncoder passwordEncoder;
     private final AuthTokenService authTokenService;
 
-    public TokenResponseDTO userLogin(@Valid AuthLoginRequestDTO authLoginRequestDTO) {
+    public AuthTokenResponseDTO userLogin(@Valid AuthLoginRequestDTO authLoginRequestDTO) {
         if (!authMapper.existsByEmail(authLoginRequestDTO.userEmail())) {
             throw ApplicationException.from(AuthErrorCode.EMAIL_NOT_FOUND);
         }
