addf911d3ee514db4b9442a4bfb0f832a73cfd14
장예연 <sudpdj124@gmail.com>
2026-08-02 22:23:28 +0900

    BUT-27 취업 준비 정보 조회 API 구현한다 (#48)
    * ãBUT-24 Feature: 취업 준비 정보 등록 ãAPI 구현
    
    * BUT-24 Feature: 취업 준비 정보 등록/수정 통합 및 예외처리
    
    * BUT-24 Refactor: Controller 응답을 UserPKResponseDTO로 변경
    
    * BUT-24 Refactor: ControVa@Valid ì추가
    
    * BUT-24 Refactor: CMapper 축약어(ep) 풀어서 작성
    
    * BUT-24 Refactor: VO 생성 로직 VO 내부로 이동 및 변수명 수정
    
    * BUT-24 Refactor: DTO에 Swagger 및 검증 어노테이션 추가
    
    * BUT-24 Refactor: Mapper 변수명 축약어 수정
    
    * BUT-27 Feature: 취업 준비 정보 조회 API 구현
    
    * BUT-24 Refactor: Service가 VO를 리턴하도록 변경
    
    * BUT-24 Test: 취업 준비 정보 저장 테스트코드 작성
    
    * BUT-27 Test: 취업 준비 정보 조회 테스트코드 작성
    
    * BUT-27 Refactor: 불필요한 코드 제거
    
    * BUT-27 Refactor: 테이블명/컬럼명 변경 반영
    
    * BUT-27 Refactor: @AuthUser적용 ë및 테스트

--- src/main/java/com/talented/buttie/user/controller/UserController.java
+++ src/main/java/com/talented/buttie/user/controller/UserController.java
@@ -1,19 +1,24 @@
 package com.talented.buttie.user.controller;
 
 import com.talented.buttie.common.response.ApplicationResponse;
+import com.talented.buttie.common.security.AuthenticationUser;
+import com.talented.buttie.common.security.annotation.AuthUser;
 import com.talented.buttie.common.util.PKCrypto;
 import com.talented.buttie.user.domain.EmploymentPreparationVO;
 import com.talented.buttie.user.dto.request.UpdateEmploymentPreparationRequestDTO;
 import com.talented.buttie.user.dto.response.UserPKResponseDTO;
 import com.talented.buttie.user.service.UserService;
+import com.talented.buttie.user.dto.response.GetEmploymentPreparationResponseDTO;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import javax.validation.Valid;
 @Api(tags = "User")
 @RestController
@@ -31,5 +36,15 @@ public class UserController {
     ) {
         EmploymentPreparationVO employmentPreparation = userService.saveEmploymentPreparation(userId, request);
         return ApplicationResponse.onSuccess(new UserPKResponseDTO(PKCrypto.encrypt(employmentPreparation.getUserId())));
+    }
+
+    @GetMapping("/employment-preparation")
+    @ApiOperation("취업 준비 정보 조회")
+    public ApplicationResponse<GetEmploymentPreparationResponseDTO> getEmploymentPreparation(
+        @AuthUser AuthenticationUser user
+    ){
+        Long targetUserId = user.userId();
+        EmploymentPreparationVO employmentPreparation = userService.getEmploymentPreparation(targetUserId);
+        return ApplicationResponse.onSuccess(GetEmploymentPreparationResponseDTO.from(employmentPreparation));
     }
 }
