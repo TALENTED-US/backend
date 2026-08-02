425fbb7fb391083f392b6b244919438ca2de87bf
장예연 <sudpdj124@gmail.com>
2026-08-02 16:23:59 +0900

    BUT-58 Refactor: 취업 준비 정보 수정 API 리팩토링 및 최신화 반영

--- src/main/java/com/talented/buttie/user/controller/UserController.java
+++ src/main/java/com/talented/buttie/user/controller/UserController.java
@@ -2,16 +2,17 @@ package com.talented.buttie.user.controller;
 
 import com.talented.buttie.common.response.ApplicationResponse;
 import com.talented.buttie.common.util.PKCrypto;
-import com.talented.buttie.user.domain.EmploymentPreparationVO;
 import com.talented.buttie.user.dto.request.UpdateEmploymentPreparationRequestDTO;
 import com.talented.buttie.user.dto.response.UserPKResponseDTO;
 import com.talented.buttie.user.service.UserService;
+import com.talented.buttie.common.security.annotation.AuthUser;
+import com.talented.buttie.common.security.AuthenticationUser;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PatchMapping;
 import javax.validation.Valid;
@@ -25,11 +26,13 @@ public class UserController {
     private final UserService userService;
 
     @PatchMapping("/employment-preparation")
-    public ApplicationResponse<UserPKResponseDTO> saveEmploymentPreparation(
-        @RequestParam Long userId,
+    @ApiOperation("취업 준비 정보 수정")
+    public ApplicationResponse<UserPKResponseDTO> modifyEmploymentPreparation(
+        @AuthUser AuthenticationUser user,
         @Valid @RequestBody UpdateEmploymentPreparationRequestDTO request
     ) {
-        EmploymentPreparationVO employmentPreparation = userService.saveEmploymentPreparation(userId, request);
-        return ApplicationResponse.onSuccess(new UserPKResponseDTO(PKCrypto.encrypt(employmentPreparation.getUserId())));
+        Long targetUserId = user.userId();
+        Long userId = userService.modifyEmploymentPreparation(targetUserId, request);
+        return ApplicationResponse.onSuccess(new UserPKResponseDTO(PKCrypto.encrypt(userId)));
     }
 }
