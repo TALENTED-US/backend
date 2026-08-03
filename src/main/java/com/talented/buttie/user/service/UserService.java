b9ced6aea042c0130ba9b6ee8863f618d123cdbd
장예연 <sudpdj124@gmail.com>
2026-08-03 14:55:13 +0900

    BUT-38 회원 프로필 수정 API를 구현한다. (#52)
    * BUT-37 Feature: 회원 프로필 조회 API 구현
    
    * BUT-38 Feature: 회원 프로필 수정 API 구현
    
    * BUT-38 Feature: 회원 정보 수정 API 구현 및 테스트
    
    * Chore: 불필요한 자바 에러 로그 파일 제거 ë
    
    * BUT-38 Refactor: ãUSER 도메인 컬럼 명 변경 반영
    
    * BUT-38 Refactor: 코드변수명 대소문자 수정
    
    * BUT-38 Fix: devlop 머지 충돌 해결
    
    * BUT-38 Refactor: 리뷰 반영하여 변수명 변경(updateUserId)

--- src/main/java/com/talented/buttie/user/service/UserService.java
+++ src/main/java/com/talented/buttie/user/service/UserService.java
@@ -1,25 +1,35 @@
 package com.talented.buttie.user.service;
 
+import com.talented.buttie.common.exception.ApplicationException;
 import com.talented.buttie.user.domain.EmploymentPreparationVO;
+import com.talented.buttie.user.domain.UserProfileVO;
+import com.talented.buttie.user.domain.UserVO;
+import com.talented.buttie.user.dto.request.ModifyUserProfileRequestDTO;
 import com.talented.buttie.user.dto.request.UpdateEmploymentPreparationRequestDTO;
+import com.talented.buttie.user.exception.UserErrorCode;
 import com.talented.buttie.user.mapper.EmploymentPreparationMapper;
+import com.talented.buttie.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import com.talented.buttie.common.exception.ApplicationException;
-import com.talented.buttie.user.domain.UserProfileVO;
-import com.talented.buttie.user.exception.UserErrorCode;
-import com.talented.buttie.user.mapper.UserMapper;
-
-
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final EmploymentPreparationMapper employmentPreparationMapper;
+
     private final UserMapper userMapper;
+    private final EmploymentPreparationMapper employmentPreparationMapper;
 
-    public EmploymentPreparationVO saveEmploymentPreparation(Long userId, UpdateEmploymentPreparationRequestDTO request) {
+    public UserProfileVO getUserProfile(Long userId) {
+        UserProfileVO userProfile = userMapper.selectUserProfile(userId);
 
+        if (userProfile == null) {
+            throw ApplicationException.from(UserErrorCode.USER_NOT_FOUND);
+        }
+
+        return userProfile;
+    }
+
+    public Long modifyEmploymentPreparation(Long userId, UpdateEmploymentPreparationRequestDTO request) {
         EmploymentPreparationVO employmentPreparation =
             EmploymentPreparationVO.createEmploymentPreparation(userId, request);
 
@@ -27,7 +37,7 @@ public class UserService {
         if (updated == 0) {
             throw ApplicationException.from(UserErrorCode.EMPLOYMENT_PREPARATION_NOT_FOUND);
         }
-        return employmentPreparation;
+        return userId;
     }
     public EmploymentPreparationVO getEmploymentPreparation(Long userId) {
         EmploymentPreparationVO employmentPreparation =
@@ -40,14 +50,18 @@ public class UserService {
         return employmentPreparation;
     }
 
+    public Long modifyUserProfile(Long userId, ModifyUserProfileRequestDTO request) {
+        if (userMapper.countByNickname(request.nickname()) > 0) {
+            throw ApplicationException.from(UserErrorCode.DUPLICATE_NICKNAME);
+        }
 
-    public UserProfileVO getUserProfile(Long userId) {
-        UserProfileVO vo = userMapper.selectUserProfile(userId);
+        UserVO user = UserVO.createModifiedUser(userId, request.nickname());
+        int updatedUserId = userMapper.updateUser(user);
 
-        if (vo == null) {
+        if (updatedUserId == 0) {
             throw ApplicationException.from(UserErrorCode.USER_NOT_FOUND);
         }
 
-        return vo;
+        return userId;
     }
 }
