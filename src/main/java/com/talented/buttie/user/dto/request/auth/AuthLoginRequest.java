8fcd61c2f38a6ce2431b6979868345229f4bbcce
hurrayPersimmon <rahel337@naver.com>
2026-08-06 22:23:58 +0900

    BUT-70 Chore: Swagger 변경

--- src/main/java/com/talented/buttie/user/dto/request/auth/AuthLoginRequest.java
+++ src/main/java/com/talented/buttie/user/dto/request/auth/AuthLoginRequest.java
@@ -3,13 +3,13 @@ package com.talented.buttie.user.dto.request.auth;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
-@ApiModel(description = "로그인 요청 DTO")
+@ApiModel("로그인 요청 DTO")
 public record AuthLoginRequest(
 
-    @ApiModelProperty(value = "사용자 이메일", example = "abc@example.com")
+    @ApiModelProperty(value = "사용자 이메일", example = "gildong@example.com")
     String userEmail,
 
-    @ApiModelProperty(value = "비밀번호", example = "Password123!")
+    @ApiModelProperty(value = "비밀번호", example = "Sun1!2026")
     String password
 ) {
 }
