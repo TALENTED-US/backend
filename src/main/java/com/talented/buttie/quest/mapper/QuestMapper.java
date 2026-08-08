47fb4f5ea873e74bc4a2194450fe8b89ec4fc4a1
M00NJK <moonjikwon@naver.com>
2026-08-08 09:29:36 +0900

    BUT-101 Refactor: 내 행동 과제 목록을 조회하는 API를 구현한다.
    - QuestMapper에서 미사용 쿼리문 삭제 및 코드 리팩토링

--- src/main/java/com/talented/buttie/quest/mapper/QuestMapper.java
+++ src/main/java/com/talented/buttie/quest/mapper/QuestMapper.java
@@ -1,6 +1,5 @@
 package com.talented.buttie.quest.mapper;
 
-import com.talented.buttie.quest.domain.QuestStatus;
 import com.talented.buttie.quest.domain.QuestVO;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
@@ -9,18 +8,7 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface QuestMapper {
 
-    void save(QuestVO quest);
-
     List<QuestVO> findAllByUserId(@Param("userId") Long userId);
 
-    List<QuestVO> findAllWithItemByUserId(@Param("userId") Long userId);
-
     List<QuestVO> findAllBySimulationId(@Param("simulationId") Long simulationId);
-
-    QuestVO findById(@Param("questId") Long questId);
-
-    void updateStatus(
-        @Param("questId") Long questId,
-        @Param("questStatus") QuestStatus questStatus
-    );
 }
