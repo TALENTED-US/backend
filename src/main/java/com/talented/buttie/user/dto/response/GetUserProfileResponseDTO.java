e9d70763e3f17c59d6a2fceeb88ce0fff96ebad0
장예연 <sudpdj124@gmail.com>
2026-08-09 18:19:43 +0900

    BUT-117 마이페이지 내정보 조회 API 구현 (#150)
    * BUT-117 Feature: 마이페이지 내정보 조회 API 구현
    
    * BUT-117 Chore: API를 user/my/info로 변경

--- src/main/java/com/talented/buttie/user/dto/response/GetUserProfileResponseDTO.java
+++ src/main/java/com/talented/buttie/user/dto/response/GetUserProfileResponseDTO.java
@@ -1,5 +1,6 @@
 package com.talented.buttie.user.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.talented.buttie.user.domain.UserProfileVO;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -8,34 +9,33 @@ import java.time.LocalDate;
 @ApiModel(description = "회원 프로필 조회 응답")
 public record GetUserProfileResponseDTO(
 
-
-    @ApiModelProperty(value = "닉네임", example = "김재준")
-    String userNickname,
-
-    @ApiModelProperty(value = "버티 누적 경험치", example = "340")
-    Integer buttieTotalExp,
+    @ApiModelProperty(value = "버티 이미지 URL", example = "https://cdn.buttie.com/buttie/lv1_stable.png")
+    String buttieImageUrl,
 
     @ApiModelProperty(value = "버티 레벨", example = "5")
     Integer buttieLevel,
 
+    @ApiModelProperty(value = "닉네임", example = "김재준")
+    String userNickname,
+
     @ApiModelProperty(value = "이름", example = "김민서")
     String userName,
 
-    @ApiModelProperty(value = "생년월일", example = "2026-07-30")
+    @ApiModelProperty(value = "생년월일", example = "1998-03-15")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     LocalDate birthDate,
 
     @ApiModelProperty(value = "휴대전화 번호", example = "010-1234-5678")
     String userPhoneNumber,
 
     @ApiModelProperty(value = "이메일", example = "qwert1234@gmail.com")
-
     String userEmail
 ) {
     public static GetUserProfileResponseDTO from(UserProfileVO vo) {
         return new GetUserProfileResponseDTO(
-            vo.getUserNickname(),
-            vo.getButtieTotalExp(),
+            vo.getButtieImageUrl(),
             vo.getButtieLevel(),
+            vo.getUserNickname(),
             vo.getUserName(),
             vo.getBirthDate(),
             vo.getUserPhoneNumber(),
