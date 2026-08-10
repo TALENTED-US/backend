4e7dd1662c0a1247df6bb35af028f580f830e0dd
hurrayPersimmon <rahel337@naver.com>
2026-08-10 14:09:07 +0900

    BUT-127 Fix: AuthUser 삭제, PathVariable 추가

--- src/main/java/com/talented/buttie/user/controller/AuthController.java
+++ src/main/java/com/talented/buttie/user/controller/AuthController.java
@@ -30,6 +30,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -64,11 +65,10 @@ public class AuthController {
     }
 
     @ApiOperation("사용자 동의")
-    @PostMapping("/consent")
+    @PostMapping("/consent/{userId}")
     public ApplicationResponse<UserPKResponse> createUserConsent(
-        @AuthUser AuthenticationUser authUser
+        @PathVariable Long userId
     ) {
-        Long userId = authUser.userId();
         Long targetUserId = authCreateService.createUserConsent(userId);
         return ApplicationResponse.onSuccess(new UserPKResponse(PKCrypto.encrypt(targetUserId)));
     }
