fb3d7b637c8ddec5abcabe01e2424ea9bcffa28c
ahhyunii <153539917+ahhyunii@users.noreply.github.com>
2026-08-10 13:43:44 +0900

    BUT-68 Feature: 확정 시뮬레이션 삭제 API를 구현 (#166)
    - DELETE /api/simulation/confirmed API 추가
    - Service, Mapper에서 사용자의 최근 확정 시뮬레이션 조회 후 삭제 구현
     #84

--- src/main/java/com/talented/buttie/simulation/service/SimulationDeleteService.java
+++ src/main/java/com/talented/buttie/simulation/service/SimulationDeleteService.java
@@ -1,5 +1,31 @@
 package com.talented.buttie.simulation.service;
 
+import com.talented.buttie.common.exception.ApplicationException;
+import com.talented.buttie.simulation.domain.SimulationVO;
+import com.talented.buttie.simulation.exception.SimulationErrorCode;
+import com.talented.buttie.simulation.mapper.SimulationMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class SimulationDeleteService {
 
+    private final SimulationMapper simulationMapper;
+
+    @Transactional
+    public void deleteSimulation(Long userId) {
+        SimulationVO confirmedSimulation = simulationMapper.findLatestConfirmedByUserId(userId);
+
+        if(confirmedSimulation == null) {
+            throw ApplicationException.from(SimulationErrorCode.CONFIRMED_SIMULATION_NOT_FOUND);
+        }
+
+        int deleterows = simulationMapper.deleteById(confirmedSimulation.getSimulationId());
+
+        if(deleterows == 0) {
+            throw ApplicationException.from(SimulationErrorCode.CONFIRMED_SIMULATION_NOT_FOUND);
+        }
+    }
 }
