71b505ba7d307b52013879968cf6361a8d774f64
hurrayPersimmon <rahel337@naver.com>
2026-08-12 17:02:14 +0900

    BUT-134 feat: 마이데이터 자산과 거래 동기화 API 분리

--- src/main/java/com/talented/buttie/mydata/dto/response/MydataAssetRegistrationResponse.java
+++ src/main/java/com/talented/buttie/mydata/dto/response/MydataAssetRegistrationResponse.java
@@ -1,14 +1,7 @@
 package com.talented.buttie.mydata.dto.response;
 
-import lombok.Builder;
-
-@Builder
 public record MydataAssetRegistrationResponse(
     int registeredAccountCount,
-    int registeredCardCount,
-    int insertedTransactionCount,
-    int skippedTransactionCount,
-    int excludedDuplicateCount,
-    int fixedExpenseCandidateCount
+    int registeredCardCount
 ) {
 }
